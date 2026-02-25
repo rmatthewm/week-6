3c860c84fdd24ff2a7a2485d48547f0a61d8dcfe
mosleym <mosleym@iu.edu>
2026-02-25 17:18:51 -0500

    removed accidental AI stuff

--- apputil.py
+++ apputil.py
@@ -13,40 +13,4 @@ class Genius:
         self.access_token = access_token
 
     # Exercise 2
-    def get_artist(self, search_term):
-        """Return the artist information dictionary for *search_term*.
-
-        The Genius search endpoint returns a list of song hits; the primary
-        artist for the first hit is used to pick an artist ID.  That ID is
-        then supplied to the ``/artists/{id}`` endpoint (see the Genius
-        documentation under **Artists**).  The method returns the artist
-        dictionary obtained from the JSON response.
-
-        If there are no hits the method returns ``None``.
-        """
-        base = "http://api.genius.com"
-
-        # first do a search to locate a possible artist
-        search_url = f"{base}/search"
-        params = {"q": search_term, "access_token": self.access_token}
-        resp = requests.get(search_url, params=params)
-        resp.raise_for_status()
-        data = resp.json()
-
-        hits = data.get("response", {}).get("hits", [])
-        if not hits:
-            # nothing found
-            return None
-
-        # grab the primary artist id from the first hit
-        artist_id = hits[0]["result"]["primary_artist"]["id"]
-
-        # now hit the artists endpoint
-        artist_url = f"{base}/artists/{artist_id}"
-        resp2 = requests.get(artist_url, params={"access_token": self.access_token})
-        resp2.raise_for_status()
-        artist_data = resp2.json()
-
-        # return the dictionary containing the artist information
-        return artist_data.get("response", {}).get("artist")
-
+    
