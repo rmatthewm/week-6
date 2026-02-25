5652b1ab8cdd4d8421536d47dadea1ee23692b9f
mosleym <mosleym@iu.edu>
2026-02-25 17:41:00 -0500

    finished exercise 2

--- apputil.py
+++ apputil.py
@@ -14,3 +14,34 @@ class Genius:
 
     # Exercise 2
     def get_artist(self, search_term):
+        # Base url
+        base_url = 'http://api.genius.com'
+        # Make a search query
+        genius_search_url = f"{base_url}/search?q={search_term}"
+
+        response = requests.get(genius_search_url, 
+            headers={"Authorization": "Bearer " + ACCESS_TOKEN})
+
+        json_data = response.json()
+
+        # Get the first result if there is one
+        if len(json_data['response']['hits']) == 0:
+            return None
+
+        first_result = json_data['response']['hits'][0]
+
+        # Get the artist id
+        artist_id = first_result['result']['primary_artist']['id']
+
+        # Make another query for the artist info
+        genius_artist_url = f"{base_url}/artists/{artist_id}"
+        response = requests.get(genius_artist_url, 
+            headers={"Authorization": "Bearer " + ACCESS_TOKEN})
+
+        # Return the json data
+        return response.json()
+
+
+# testing
+g = Genius(ACCESS_TOKEN)
+print(g.get_artist('Katy Perry')['response']['artist']['name'])
