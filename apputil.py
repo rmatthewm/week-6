43fe2551d006a7358d492dd8b34c19ec7d2f0c87
mosleym <mosleym@iu.edu>
2026-02-25 20:50:12 -0500

    more refactoring

--- apputil.py
+++ apputil.py
@@ -6,42 +6,76 @@ load_dotenv()
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 
 
-# Exercise 1
 class Genius:
+    """ A wrapper for the Genius API. """
+
     def __init__(self, access_token):
-        # Store the token so other methods can use it
-        self.access_token = access_token
+        """ Constructor, takes access token to be able to access the Genius API.
+
+        Args:
+            access_token (str): the access token 
+        """
+        # The access toke to use
+        self.__access_token = access_token
         
         # Base url of the Genius API
-        self.base_url = 'http://api.genius.com'
+        self.__base_url = 'http://api.genius.com'
 
-    # Exercise 2
-    def get_artist(self, search_term):
-        # Make a search query
-        genius_search_url = f"{self.base_url}/search?q={search_term}"
+    def get_artist_id(self, search_term):
+        """ Returns the artist id from Genius given a search term.
 
-        response = requests.get(genius_search_url, 
-            headers={"Authorization": "Bearer " + self.access_token})
+        Args:
+            search_term (str): the search term to look for the artist
+
+        Returns:
+            int: the artist id, or None if no artist is found
+        """
+        # Make a search query to find the songs with the desired artist 
+        search_url = f"{self.__base_url}/search?q={search_term}"
+        response = requests.get(search_url, 
+            headers={"Authorization": "Bearer " + self.__access_token})
 
         json_data = response.json()
 
-        # Get the first result if there is one
+        # If there are no results, return None
         if len(json_data['response']['hits']) == 0:
             return None
 
+        # Otherwise, get the first result
         first_result = json_data['response']['hits'][0]
 
-        # Get the artist id
-        artist_id = first_result['result']['primary_artist']['id']
+        # Return the artist id
+        return first_result['result']['primary_artist']['id']
 
-        # Make another query for the artist info
-        genius_artist_url = f"{self.base_url}/artists/{artist_id}"
-        response = requests.get(genius_artist_url, 
-            headers={"Authorization": "Bearer " + self.access_token})
+
+    def get_artist(self, search_term):
+        """ Returns the artist information from Genius given a search term.
+
+        Args:
+            search_term (str): the search term to look for the artist
+
+        Returns:
+            dict: the 'JSON' data in Python format with the artist info, empty if no result
+        """
+
+        # Get the artist id
+        artist_id = self.get_artist_id(search_term)
+        
+        # If there are no results, return an empty dictionary
+        if artist_id is None:
+            return {}
+
+        # Make another query to get the artist info
+        artist_url = f"{self.__base_url}/artists/{artist_id}"
+        response = requests.get(artist_url, 
+            headers={"Authorization": "Bearer " + self.__access_token})
 
         # Return the json data
         return response.json()['response']['artist']
 
+    def get_artists(self, search_term):
+        pass
+       
 # testing
 g = Genius(ACCESS_TOKEN)
 print(g.get_artist('Radiohead'))
